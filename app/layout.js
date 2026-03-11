174244603cf0ff1b3b7521defc27fd9b9506b538
fm93radio <florarezo@automaias.com>
2026-03-11 12:02:55 -0300

    fix: viewport mobile

--- app/layout.js
+++ app/layout.js
@@ -7,7 +7,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="es">
+    <html lang="es">`n      <head>`n        <meta name="viewport" content="width=device-width, initial-scale=1" />`n      </head>
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
